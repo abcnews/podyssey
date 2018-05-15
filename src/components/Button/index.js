4844bb5e752ba3b2186caed1f73dc5ad833739fc
Colin Gourlay <Gourlay.Colin@abc.net.au>
2018-05-15 15:24:13 +1000

    Attempt to make Buttons not allow page zooming on touch devices when double-tapped.

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -3,13 +3,47 @@ const { h, Component } = require('preact');
 const Icon = require('../Icon');
 const styles = require('./styles.css');
 
-module.exports = ({ type, iconProps = {}, onClick, className, children, ...props }) => {
-  iconProps.type = iconProps.type || type;
+const MAX_TIME_DIFF = 500;
 
-  return (
-    <button className={cn(styles.root, className)} data-type={type ? type : null} onClick={onClick} {...props}>
-      {type && <Icon {...iconProps} />}
-      {children && children.length > 0 && <span>{children}</span>}
-    </button>
-  );
-};
+class Button extends Component {
+  constructor(props) {
+    super(props);
+
+    this.lastTapAt = 0;
+    this.preventDoubleTapZoom = this.preventDoubleTapZoom.bind(this);
+  }
+
+  preventDoubleTapZoom(event) {
+    if (event.touches.length > 1) {
+      return;
+    }
+
+    const tapAt = Date.now();
+
+    if (tapAt - this.lastTapAt < MAX_TIME_DIFF) {
+      event.preventDefault();
+      event.currentTarget.click();
+    }
+
+    this.lastTapAt = tapAt;
+  }
+
+  render({ type, iconProps = {}, onClick, className, children, ...props }) {
+    iconProps.type = iconProps.type || type;
+
+    return (
+      <button
+        className={cn(styles.root, className)}
+        data-type={type ? type : null}
+        onClick={onClick}
+        onTouchStart={this.preventDoubleTapZoom}
+        {...props}
+      >
+        {type && <Icon {...iconProps} />}
+        {children && children.length > 0 && <span>{children}</span>}
+      </button>
+    );
+  }
+}
+
+module.exports = Button;
