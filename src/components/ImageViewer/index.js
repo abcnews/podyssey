0095a5d9038b1731c4239059e36d6f25456ccd69
Joshua Byrd <phocks@gmail.com>
2019-07-02 13:45:25 +1000

    Divide by zero makes universe implode

--- src/components/ImageViewer/index.js
+++ src/components/ImageViewer/index.js
@@ -59,7 +59,8 @@ const transformX = (pct, ratio) => {
   const pctX = typeof pct === 'number' ? 50 - Math.min(100, Math.max(0, pct)) : 0;
   const absPctX = Math.abs(pctX);
 
-  return Math.min(absPctX, (1 - ratio) / ratio * 50) * (pctX / absPctX);
+  // Note: try not to divide by zero if pct === 50
+  return absPctX === 0 ? 0 : Math.min(absPctX, (1 - ratio) / ratio * 50) * (pctX / absPctX);
 };
 
 const animationCSS = (id, { from = 0, to = 100, duration = 10, delay = 0, infinite = false }, ratio = 1) => {
