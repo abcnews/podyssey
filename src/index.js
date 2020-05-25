a03447842dee84b4675c20d6bcb7c4619f2eda3d
Colin Gourlay <gourlay.colin@abc.net.au>
2020-05-25 13:38:06 +1000

    Hide Header-media

--- src/index.js
+++ src/index.js
@@ -1,17 +1,22 @@
-require('./polyfills');
+require("./polyfills");
 
-const { h, render } = require('preact');
-const xhr = require('xhr');
-const { IS_STANDALONE } = require('./constants');
-const { detailPageURLFromCMID, getAudioCMID, normalise, parsePlayerProps } = require('./utils');
-require('./global.css');
+const { h, render } = require("preact");
+const xhr = require("xhr");
+const { IS_STANDALONE } = require("./constants");
+const {
+  detailPageURLFromCMID,
+  getAudioCMID,
+  normalise,
+  parsePlayerProps
+} = require("./utils");
+require("./global.css");
 
-const rootEl = document.querySelector('[data-podyssey-root]');
+const rootEl = document.querySelector("[data-podyssey-root]");
 const audioDocumentCMID = getAudioCMID();
 let playerProps = null;
 
 function renderApp() {
-  const App = require('./components/App');
+  const App = require("./components/App");
 
   render(<App playerProps={playerProps} />, rootEl, rootEl.firstChild);
 }
@@ -19,38 +24,40 @@ function renderApp() {
 if (audioDocumentCMID) {
   normalise(rootEl);
   renderApp();
-  xhr({ url: detailPageURLFromCMID(audioDocumentCMID) }, (error, response, body) => {
-    if (error || response.statusCode !== 200) {
-      console.error(error || new Error(response.statusCode));
+  xhr(
+    { url: detailPageURLFromCMID(audioDocumentCMID) },
+    (error, response, body) => {
+      if (error || response.statusCode !== 200) {
+        console.error(error || new Error(response.statusCode));
+      }
+
+      playerProps = {
+        cmid: audioDocumentCMID,
+        ...parsePlayerProps(body)
+      };
+
+      renderApp();
     }
-
-    playerProps = {
-      cmid: audioDocumentCMID,
-      ...parsePlayerProps(body)
-    };
-
-    renderApp();
-  });
+  );
 }
 
 if (IS_STANDALONE) {
-  document.documentElement.setAttribute('standalone', '');
+  document.documentElement.setAttribute("standalone", "");
 }
 
 if (module.hot) {
-  module.hot.accept('./components/App', () => {
+  module.hot.accept("./components/App", () => {
     try {
       renderApp();
     } catch (err) {
-      const ErrorBox = require('./components/ErrorBox');
+      const ErrorBox = require("./components/ErrorBox");
       render(<ErrorBox error={err} />, rootEl, rootEl.firstChild);
     }
   });
 }
 
-if (process.env.NODE_ENV === 'development') {
-  window.addEventListener('podyssey:entry', ({ detail }) => {
+if (process.env.NODE_ENV === "development") {
+  window.addEventListener("podyssey:entry", ({ detail }) => {
     console.log(detail);
   });
-  require('preact/devtools');
 }
